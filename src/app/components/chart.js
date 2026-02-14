be754bdfaa7211f15d6f905aad48db14697f127d
Ivan Zotov <zia14leg@gmail.com>
2026-02-14 17:08:13 +0100

    feat: careate test chart

---
+++ src/app/components/chart.js
@@ -0,0 +1,29 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Plot = dynamic(() => import("react-plotly.js"), {
+  ssr: false,
+});
+
+export function Chart() {
+  return (
+    <Plot
+      data={[
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: "scatter",
+          mode: "lines+markers",
+          marker: { color: "red" },
+        },
+        { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
+      ]}
+      layout={{
+        width: "100%",
+        height: "100%",
+        title: { text: "A Fancy Plot" },
+      }}
+    />
+  );
+}
